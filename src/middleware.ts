b4278309b6d1e3a2efe51ef771e52adf59b62e48
Daniel Castillejo <devcastillejo@gmail.com>
2023-09-05 17:57:22 -0500

    build: token is null

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,32 +2,42 @@ import { getToken } from "next-auth/jwt"
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
-export default withAuth(
-  async function middleware(req) {
-    const token = await getToken({ req })
-    const isAuth = !!token
+// export default withAuth(
+//   async function middleware(req) {
+//     const token = await getToken({ req })
+//     const isAuth = !!token
 
-    console.log(token)
-    if (req.nextUrl.pathname === "/login") {
-      if (isAuth) {
-        return NextResponse.redirect(new URL(`/dashboard`, req.url))
-      }
-      return null
-    }
+//     console.log(token)
+//     if (req.nextUrl.pathname === "/login") {
+//       if (isAuth) {
+//         return NextResponse.redirect(new URL(`/dashboard`, req.url))
+//       }
+//       return null
+//     }
 
-    if (!isAuth) {
-      return NextResponse.redirect(new URL(`/login`, req.url))
-    }
-  },
-  {
-    callbacks: {
-      authorized() {
-        return true
-      }
+//     if (!isAuth) {
+//       return NextResponse.redirect(new URL(`/login`, req.url))
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized() {
+//         return true
+//       }
+//     }
+//   }
+// )
+
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      console.log(token)
+      // matcher array will be only requires the user to be logged in
+      return !!token
     }
   }
-)
+})
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login"]
+  matcher: ["/dashboard/:path*"]
 }
