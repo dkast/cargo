1aef1fcfe72352c4bcb1d0a0d3cbbb4989aeabdc
Daniel Castillejo <devcastillejo@gmail.com>
2023-09-05 19:59:34 -0500

    build: revert to default config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,5 @@
-import { getToken } from "next-auth/jwt"
-import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
-
-export default withAuth(
-  async function middleware(req) {
-    console.log(req)
-    const token = await getToken({ req })
-    const isAuth = !!token
-
-    if (req.nextUrl.pathname === "/login") {
-      if (isAuth) {
-        return NextResponse.redirect(new URL(`/dashboard`, req.url))
-      }
-      return null
-    }
-
-    if (!isAuth) {
-      return NextResponse.redirect(new URL(`/login`, req.url))
-    }
-  },
-  {
-    callbacks: {
-      authorized() {
-        return true
-      }
-    }
-  }
-)
+export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login"]
+  matcher: ["/dashboard/:path*"]
 }
