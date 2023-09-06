2f99cba0d9a9341ab864eef0099ec1caab562f31
Daniel Castillejo <devcastillejo@gmail.com>
2023-09-05 19:23:43 -0500

    build: debug middleware req

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,33 @@
-export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+  async function middleware(req) {
+    console.log(req)
+    const token = await getToken({ req })
+    const isAuth = !!token
+
+    if (req.nextUrl.pathname === "/login") {
+      if (isAuth) {
+        return NextResponse.redirect(new URL(`/dashboard`, req.url))
+      }
+      return null
+    }
+
+    if (!isAuth) {
+      return NextResponse.redirect(new URL(`/login`, req.url))
+    }
+  },
+  {
+    callbacks: {
+      authorized() {
+        return true
+      }
+    }
+  }
+)
 
 export const config = {
-  matcher: ["/dashboard/:path*"]
+  matcher: ["/dashboard/:path*", "/login"]
 }
