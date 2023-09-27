89b930ac13ed9600df9cdb1e93bb1e9307d0c46b
Daniel Castillejo <devcastillejo@gmail.com>
2023-09-27 00:02:24 -0500

    feat(forms): implement combobox for mobile

--- src/app/dashboard/settings/transports/combobox.tsx
+++ src/app/dashboard/settings/transports/combobox.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react"
+import React, { type ReactNode } from "react"
 import { PopoverContent } from "@radix-ui/react-popover"
 
 import {
@@ -9,25 +9,50 @@ import {
   CommandItem,
   CommandList
 } from "@/components/ui/command"
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
 import { Popover, PopoverTrigger } from "@/components/ui/popover"
+import { useMobile } from "@/lib/use-mobile"
 import { cn } from "@/lib/utils"
 
-function ComboBox({ children }: { children: ReactNode }) {
-  return <Popover>{children}</Popover>
-}
-function ComboBoxContent({ children }: { children: ReactNode }) {
-  return (
-    <PopoverContent
-      className={cn(
-        "z-50 w-72 rounded-md border border-gray-200 bg-white p-2 text-gray-950 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50"
-      )}
-    >
-      <Command>{children}</Command>
-    </PopoverContent>
-  )
+function ComboBox({
+  open,
+  setOpen,
+  children,
+  trigger
+}: {
+  open?: boolean
+  setOpen?: (open: boolean) => void
+  children: ReactNode
+  trigger: ReactNode
+}) {
+  const isMobile = useMobile()
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>{trigger}</DrawerTrigger>
+        <DrawerContent className="px-2 py-4">
+          <div className="mx-auto mb-4 h-1.5 w-12 flex-shrink-0 rounded-full bg-gray-300" />
+          <Command>{children}</Command>
+        </DrawerContent>
+      </Drawer>
+    )
+  } else {
+    return (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>{trigger}</PopoverTrigger>
+        <PopoverContent
+          className={cn(
+            "z-50 w-72 rounded-md border border-gray-200 bg-white text-gray-950 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50"
+          )}
+        >
+          <Command>{children}</Command>
+        </PopoverContent>
+      </Popover>
+    )
+  }
 }
 
-const ComboBoxTrigger = PopoverTrigger
 const ComboBoxInput = CommandInput
 const ComboBoxList = CommandList
 const ComboBoxEmpty = CommandEmpty
@@ -36,8 +61,6 @@ const ComboBoxGroup = CommandGroup
 
 export {
   ComboBox,
-  ComboBoxTrigger,
-  ComboBoxContent,
   ComboBoxInput,
   ComboBoxList,
   ComboBoxEmpty,
--- src/app/dashboard/settings/transports/test-form.tsx
+++ src/app/dashboard/settings/transports/test-form.tsx
@@ -1,29 +1,20 @@
 "use client"
 
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import {
   ComboBox,
-  ComboBoxContent,
   ComboBoxEmpty,
   ComboBoxGroup,
   ComboBoxInput,
   ComboBoxItem,
-  ComboBoxList,
-  ComboBoxTrigger
+  ComboBoxList
 } from "@/app/dashboard/settings/transports/combobox"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Check, ChevronsUpDown } from "lucide-react"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList
-} from "@/components/ui/command"
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { type companySchema } from "@/lib/types"
 import { cn } from "@/lib/utils"
@@ -41,6 +32,7 @@ export default function TestForm({
     resolver: zodResolver(testSchema),
     defaultValues: { company: "" }
   })
+  const [open, setOpen] = useState(false)
 
   return (
     <Form {...form}>
@@ -50,8 +42,10 @@ export default function TestForm({
           name="company"
           render={({ field }) => (
             <FormItem>
-              <ComboBox>
-                <ComboBoxTrigger asChild>
+              <ComboBox
+                open={open}
+                setOpen={setOpen}
+                trigger={
                   <FormControl>
                     <Button
                       variant="outline"
@@ -68,36 +62,36 @@ export default function TestForm({
                       <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                     </Button>
                   </FormControl>
-                </ComboBoxTrigger>
-                <ComboBoxContent>
-                  <ComboBoxInput placeholder="Buscar..." />
-                  <ComboBoxList>
-                    <ComboBoxEmpty className="px-2">
-                      No se encontraron resultados
-                    </ComboBoxEmpty>
-                    <ComboBoxGroup>
-                      {companies.map(company => (
-                        <ComboBoxItem
-                          value={company.name}
-                          key={company.id}
-                          onSelect={() => {
-                            form.setValue("company", company.id!)
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              company.id === field.value
-                                ? "opacity-100"
-                                : "opacity-0"
-                            )}
-                          />
-                          {company.name}
-                        </ComboBoxItem>
-                      ))}
-                    </ComboBoxGroup>
-                  </ComboBoxList>
-                </ComboBoxContent>
+                }
+              >
+                <ComboBoxInput placeholder="Buscar..." />
+                <ComboBoxList>
+                  <ComboBoxEmpty className="px-2">
+                    No se encontraron resultados
+                  </ComboBoxEmpty>
+                  <ComboBoxGroup>
+                    {companies.map(company => (
+                      <ComboBoxItem
+                        value={company.name}
+                        key={company.id}
+                        onSelect={() => {
+                          form.setValue("company", company.id!)
+                          setOpen(false)
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                            company.id === field.value
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                        {company.name}
+                      </ComboBoxItem>
+                    ))}
+                  </ComboBoxGroup>
+                </ComboBoxList>
               </ComboBox>
             </FormItem>
           )}
