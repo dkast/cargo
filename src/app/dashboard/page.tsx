daf22bff26a0cdbb24f05f5a5e6598c8e08c1b0f
Daniel Castillejo <devcastillejo@gmail.com>
2023-12-05 20:44:08 -0600

    feat: add tremor for dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,33 @@
+import { format } from "date-fns"
+import { es } from "date-fns/locale"
 import { type Metadata } from "next"
 
 import PageHeader from "@/components/dashboard/page-header"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { getCurrentUser } from "@/lib/session"
 
 export const metadata: Metadata = {
   title: "Inicio"
 }
 
-export default function DashboardPage() {
-  return <PageHeader title="Bienvenido"></PageHeader>
+export default async function DashboardPage() {
+  const user = await getCurrentUser()
+
+  if (!user) return null
+
+  const humanDate = format(new Date(), "EEEE, d MMMM yyyy", { locale: es })
+  const greeting = user.name ? `Hola, ${user.name.split(" ")[0]}` : "Hola"
+  return (
+    <>
+      <PageHeader title={greeting} description={humanDate} />
+      <section className="grid grid-cols-3 px-3 py-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Contenedores</CardTitle>
+          </CardHeader>
+          <CardContent>Hola Mundo</CardContent>
+        </Card>
+      </section>
+    </>
+  )
 }
