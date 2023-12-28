fc887d1095964a8e4856cfa1587d044bc150fa3d
Daniel Castillejo <devcastillejo@gmail.com>
2023-12-27 19:36:58 -0600

    feat: added container crud page and components

---
+++ src/app/dashboard/settings/containers/columns.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import ContainerDelete from "@/app/dashboard/settings/containers/container-delete"
+import ContainerEdit from "@/app/dashboard/settings/containers/container-edit"
+import { type ColumnDef } from "@tanstack/react-table"
+import { ChevronDown, ChevronsUpDown, ChevronUp } from "lucide-react"
+import { type z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import { actionType, type containerSchema } from "@/lib/types"
+
+export const columns: ColumnDef<z.infer<typeof containerSchema>>[] = [
+  {
+    accessorKey: "containerNbr",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Unidad
+          {{
+            asc: <ChevronUp className="ml-2 h-4 w-4" />,
+            desc: <ChevronDown className="ml-2 h-4 w-4" />
+          }[column.getIsSorted() as string] ?? (
+            <ChevronsUpDown className="ml-2 h-4 w-4" />
+          )}
+        </Button>
+      )
+    }
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const container = row.original
+
+      return (
+        <div className="flex justify-end gap-x-2">
+          <ContainerEdit
+            organizationId={container.organizationId}
+            container={container}
+            action={actionType.UPDATE}
+          />
+          <ContainerDelete data={container} />
+        </div>
+      )
+    }
+  }
+]
---
+++ src/app/dashboard/settings/containers/container-delete.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import toast from "react-hot-toast"
+import { Trash2 } from "lucide-react"
+import { useAction } from "next-safe-action/hook"
+import { type z } from "zod"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { deleteContainer } from "@/server/actions/container"
+import { type containerSchema } from "@/lib/types"
+
+export default function ContainerDelete({
+  data
+}: {
+  data: z.infer<typeof containerSchema>
+}) {
+  const { execute, reset } = useAction(deleteContainer, {
+    onExecute: () => {
+      toast.loading("Eliminando Contenedor...")
+    },
+    onSuccess: data => {
+      if (data?.failure?.reason) {
+        toast.dismiss()
+        toast.error(data.failure.reason)
+      } else if (data?.success) {
+        toast.dismiss()
+      }
+      reset()
+    },
+    onError: () => {
+      toast.error("Algo salió mal")
+      reset()
+    }
+  })
+
+  const onDeleteContainer = () => {
+    execute(data)
+  }
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Trash2 className="h-4 w-4 text-red-500" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Eliminar contenedor</AlertDialogTitle>
+          <AlertDialogDescription>
+            ¿Estás seguro de eliminar el contenedor? Esta acción no se puede
+            deshacer
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            variant="destructive"
+            onClick={() => onDeleteContainer()}
+          >
+            Eliminar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
