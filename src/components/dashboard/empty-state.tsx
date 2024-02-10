b9737931f5facd2325ece94e0ebbf745ea5fe4de
Daniel Castillejo <devcastillejo@gmail.com>
2024-02-10 00:29:29 -0600

    style: empty state

--- src/components/dashboard/empty-state.tsx
+++ src/components/dashboard/empty-state.tsx
@@ -11,11 +11,11 @@ export function EmptyState({
   return (
     <div className="flex flex-col items-center justify-center gap-y-2">
       {icon && (
-        <div className="rounded-xl bg-gradient-to-b from-orange-100 via-orange-50 to-gray-50  p-2 text-orange-400">
+        <div className="rounded-xl border border-gray-300 bg-gray-50 p-2 text-gray-400 shadow-inner shadow-white ring ring-gray-200">
           {icon}
         </div>
       )}
-      <span className="text-gray-500">{title}</span>
+      <span className="font-medium text-gray-500">{title}</span>
     </div>
   )
 }
