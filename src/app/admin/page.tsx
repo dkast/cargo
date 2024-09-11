2753e677370a6c0f69da09d898d4b42cffa27b7c
Daniel Castillejo <devcastillejo@gmail.com>
2024-09-10 20:10:27 -0500

    refactor: add waitlist table to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,53 @@
+import { Inbox } from "lucide-react"
 import type { Metadata } from "next/types"
 
+import { EmptyState } from "@/components/dashboard/empty-state"
 import PageSubtitle from "@/components/dashboard/page-subtitle"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "@/components/ui/table"
+import { getWaitList } from "@/server/fetchers"
 
 export const metadata: Metadata = {
   title: "Lista de Espera"
 }
 
-export default function Page() {
+export default async function Page() {
+  const waitlist = await getWaitList()
   return (
     <div className="mx-auto max-w-2xl grow px-4 sm:px-0">
       <PageSubtitle
         title="Lista de Espera"
         description="Registros de lista de espera"
       />
+      <Card className="mt-8">
+        <CardHeader />
+        <CardContent>
+          {waitlist.length === 0 ? (
+            <EmptyState title="No hay registros" icon={<Inbox />} />
+          ) : (
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Email</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {waitlist.map(item => (
+                  <TableRow key={item.id}>
+                    <TableCell>{item.email}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+        </CardContent>
+      </Card>
     </div>
   )
 }
